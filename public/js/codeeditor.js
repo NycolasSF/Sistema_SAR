d2a57dce6c445d753d0844652bfcd89314aa03e5
NycolasSF <nycolassilvafroes@gmail.com>
2018-08-29 22:59:46 -0400

    melhorando dashboard

--- public/js/codeeditor.js
+++ public/js/codeeditor.js
@@ -1,45 +1,16 @@
 //**https://codemirror.net/doc/manual.html**//
+var string =
+'//Criando conexão com o robo \n'+
+'var connetion = 192.168.1.204 \n' +
+'//Instanciando a Classe \n'+
+'var sar = new Robo(connetion) \n'+
+'//Passando movimentos para ele \n'+
+'sar.move(10)'
 
-//PEGANDO OS buttonS
-// var btn_monokai = document.querySelector('#btn-theme-monokai');
-// var btn_neat = document.querySelector('#btn-theme-neat');
-// //MUDAR A COR
-// var logica = false;
-// var varTheme;
-// btn_monokai.onclick = function(){
-//   logica = false;
-//   return logica;
-// }
-// btn_neat.onclick = function(){
-//   logica = true;
-// }
-//
-//
-// //INICIANDO CODEMIRROR
-// function mudar_thema() {
-//
-//   if (logica) {
-//     varTheme = 'neat'
-//     alert(theme)
-//     }else{
-//     varTheme = 'monokai'
-//   }
-
-  var editor = CodeMirror(document.querySelector('#codeeditor'), {
-    mode: "javascript",
-    value: "var robo = new Robo(connetion)",
-    tabSize: 5,
-    lineNumbers: true,
-    theme: 'monokai'
-  });
-// }
-
-//
-// function verificar() {
-//   var value = editor.getValue();
-//   if (value.length == 0) {
-//     alert('Digite uma linha de comando');
-//   }else{
-//     document.querySelector('#alert').innerHTML = 'RODANDO...';
-//   }
-// }
+var editor = CodeMirror(document.querySelector('#codeeditor'), {
+  mode: "javascript",
+  value: string,
+  tabSize: 5,
+  lineNumbers: true,
+  theme: 'monokai'
+});
