cec9e529ca273dc2b208d911964c9762b1a3cc61
NycolasSF <nycolassilvafroes@gmail.com>
2018-11-18 00:22:28 -0300

    arrumando CodeMirror

--- public/js/codeeditor.js
+++ public/js/codeeditor.js
@@ -1,19 +1,66 @@
 //**https://codemirror.net/doc/manual.html**//
-var string =
-'//Criando conexão com o robo \n'+
-'var connetion = 192.168.1.204 \n' +
-'//Instanciando a Classe \n'+
-'var sar = new Robo(connetion) \n'+
-'//Passando movimentos para ele \n'+
-'sar.move(10)'
 function criarCode() {
   abrirCode()
-  var editor = CodeMirror(document.querySelector('#codeeditor'), {
-    mode: "javascript",
-    value: string,
-    tabSize: 5,
-    lineNumbers: true,
-    theme: 'monokai'
-  });
+  $('textarea').each(function(elem) {
+    var textArea = this;
+    var string =
+    "//Criando conexão com o robo \n"+
+    "var connetion = '0.0.0.0' \n" +
+    "//Instanciando a Classe \n"+
+    "var sar = new Robo(connetion) \n"+
+    "//Passando movimentos para ele \n"+
+    "sar.move()";
 
+    textArea.value = string
+
+    var codeMirrorOpts = {
+        lineNumbers: true,
+        mode: "javascript",
+        tabSize: 5,
+        lineNumbers: true,
+        theme: 'monokai',
+        scrollbarStyle: "overlay",
+        extraKeys: {
+        "F11": function(cm) {
+          cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+        },
+        "Esc": function(cm) {
+          if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+        }
+      }
+    }
+
+    if ($(textArea).hasClass('html') || $(textArea).hasClass('script')) {
+        codeMirrorOpts.mode = {name: 'xml', htmlMode: true}
+      } else if ($(textArea).hasClass('js')) {
+        codeMirrorOpts.matchBrackets = true;
+    }
+
+    var editor = CodeMirror.fromTextArea(textArea, codeMirrorOpts);
+
+    $('#lightTheme').click(function(){
+      editor.setOption("theme", 'neat');
+    });darkTheme
+    $('#darkTheme').click(function(){
+      editor.setOption("theme", 'monokai');
+    });
+
+    if ($(textArea).hasClass('runnable')) {
+      var button = $('<button class="btn run-button">Run!</button>');
+      button.click(function() {
+        var code = editor.getValue();
+        if ($(textArea).hasClass('html')) {
+          $('#demo').html(code);
+        } else if ($(textArea).hasClass('script')) {
+          $('body').append(code);
+        } else if ($(textArea).hasClass('js')) {
+          window.eval(code);
+          window.onerror =  function(e, url, line){
+            console.log('tem erro');
+          }
+        }
+      });
+      $(this).after(button);
+    }
+  });
 }
